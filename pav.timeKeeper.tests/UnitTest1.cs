c7e8779d6483df49cfd31e0be197b7604c5eb4b2
Pawel Ciucias <pawel.ciucias@outlook.com>
2019-04-02 10:37:21 +0200

    initial set up

---
+++ pav.timeKeeper.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace pav.timeKeeper.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
